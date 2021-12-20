ff27c5a26bc0e452f6ac3fc4c0c549fa679192f7
elogeek <elodiechristin@gmail.com>
2021-12-20 15:36:06 +0100

    suite eval

--- assets/ts/components/project.ts
+++ assets/ts/components/project.ts
@@ -4,19 +4,32 @@
 export default class Project {
 
     public titleName: string;
+    public parent: HTMLElement;
 
-    constructor(N: string) {
+    constructor( N: string) {
+        this.parent = document.querySelector('.container') as HTMLElement;
         this.titleName = N;
-
+        this.build();
     }
+
 
     /**
      *  Build a project
      */
     build() {
-        const parent = document.querySelector(".container") as HTMLElement;
+
+        let div = document.createElement('div');
+        let divTitle = document.createElement('div');
+        let h2 = document.createElement('h2');
+
+        div.className = "project";
+        divTitle.className = "title";
+        this.parent.appendChild(div);
+        div.appendChild(divTitle);
+        divTitle.appendChild(h2);
 
     }
 
-
 }
+
+
--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,14 +1,3 @@
+import Project from "./components/project";
 
-
-const a :Project = new Project('pro');
-
-/**
- *  Add a project in the homePage
- */
-const btn = document.querySelector('button[type=submit]') as HTMLButtonElement;
-btn.addEventListener('click',function() {
-    let input = document.querySelector('#addTitleP') as HTMLInputElement;
-    console.log("project : " + input.value);
-
-})
-
+let x = new Project( 'test');
