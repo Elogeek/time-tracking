a6a528afbaf15974f7f034720ab9342e14a467cd
elogeek <elodiechristin@gmail.com>
2021-12-28 11:51:36 +0100

    suite eval

--- assets/ts/components/Project.ts
+++ assets/ts/components/Project.ts
@@ -6,61 +6,6 @@ import {IconsTask} from "./IconsTask";
 
 export class Project {
 
-    public titleName: string;
-    public parent: HTMLElement;
-
-    constructor( N: string) {
-        this.parent = document.querySelector('.container') as HTMLElement;
-        this.titleName = N;
-        this.build();
-        this.add();
-    }
-
-
-    /**
-     *  Build a project
-     */
-    build() {
-
-        let div = document.createElement('div');
-        let divTitle = document.createElement('div');
-        let h2 = document.createElement('h2');
-
-
-        div.className = "project";
-        divTitle.className = "title";
-
-        new IconsTask(div);
-        this.parent.appendChild(div);
-        div.appendChild(divTitle);
-        divTitle.appendChild(h2);
-
-    }
-
-    add() {
-        let btn = document.querySelector("#btnAdd") as HTMLButtonElement;
-        let array =  [] = [localStorage.getItem('projects')];
-
-            btn.addEventListener('click', function (e) {
-
-                let input = document.querySelector('.inputTitle') as HTMLInputElement;
-
-                input.addEventListener("keypress",(evt) => {
-
-                    if(evt.key === 'Enter') {
-                        let t = document.querySelector(".newTitle") as HTMLElement;
-                        t.innerHTML = input.value;
-                        console.log(input.value);
-                        return t;
-                    }
-
-                    array.push(input.value);
-                    localStorage.setItem('newProject',JSON.stringify(array));
-                })
-
-            })
-        console.log('merde, une erreur est survenu !');
-    }
 
 }
 
