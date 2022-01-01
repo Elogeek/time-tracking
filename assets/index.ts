14247ec2fdc72dc1e203f6a125887168fb59a529
elogeek <elodiechristin@gmail.com>
2022-01-01 12:10:37 +0100

    suite eval

--- assets/index.ts
+++ assets/index.ts
@@ -2,8 +2,6 @@ import Storage from "./ts/components/Storage";
 import { v4 as uuidv4 } from 'uuid';
 
 const uneConfig = Storage.getConfiguration('hello world');
-console.log(uneConfig);
 
-
+// Test du module uuid
 console.log(uuidv4());
-uuidv4();
