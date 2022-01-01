304966f539f0ba4423e83e45baa6988c4c8a61a7
elogeek <elodiechristin@gmail.com>
2022-01-01 13:45:07 +0100

    suite eval

--- assets/index.ts
+++ assets/index.ts
@@ -1,7 +1,12 @@
 import Storage from "./ts/components/Storage";
 import { v4 as uuidv4 } from 'uuid';
 
-const uneConfig = Storage.getConfiguration('hello world');
+const uneConfig = Storage.getConfiguration('hello world',uuidv4());
 
 // Test du module uuid
 console.log(uuidv4());
+
+// Test
+let a = document.querySelector(".container-project") as HTMLDivElement;
+// @ts-ignore
+a = Storage.clear();
--- assets/ts/components/Storage.ts
+++ assets/ts/components/Storage.ts
@@ -5,7 +5,25 @@ import { v4 as uuidv4 } from 'uuid';
  */
 export default class Storage {
 
-    public get(key: string): void {
+    constructor(projectName: string, ident: string) {}
+
+    /**
+     * Create a key in the localstorage
+     */
+    public static createStore(key: string): Storage {
+        localStorage.setItem(uuidv4(),"");
+        // @ts-ignore
+        return Storage;
+    }
+
+    public get(key: string): Storage {
+       // If key exist
+        // @ts-ignore
+        return Storage;
+        // If not key exist alors
+        Storage.createStore(uuidv4());
+        // @ts-ignore
+        return new Storage();
 
     }
 
@@ -19,24 +37,40 @@ export default class Storage {
      * Delete a task in the project
      * @param key
      */
-    public delete(key:string):void{}
+    public delete(key:string):void{
+
+
+
+    }
 
     /**
      * Clear Localstorage (click trash in the project) and delete project
      * @param key
      */
-    public clear(key:string):void{}
+    public  static clear(key:string):void {
+        // @ts-ignore
+        let btnTrash = document.querySelector(".icons ").firstChild as HTMLElement;
+        let containerProject = document.querySelector(".container-project") as HTMLDivElement;
+
+        btnTrash.addEventListener("click", (ev => {
+            localStorage.get(uuidv4());
+            localStorage.clear();
+            containerProject.remove();
+            containerProject.style.display = "none";
+        }))
+    }
 
     /**
      * Config pour tous les projects
      * @param projectName
+     * @param ident
      */
-    public static getConfiguration(projectName: string, ident: number): Storage
+    public static getConfiguration(projectName: string, ident: string): Storage
     {
         // Vérifier si la clé existe dans le storage, si oui,
         // For random ident to be sure not to get the wrong project
-        return new Storage(uuidv4());
-        console.log(uuidv4());
+        return new Storage(" ",uuidv4());
+
 
 
 
